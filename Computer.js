e7595aaea17228cd3d3e6aed00d35f937eeaf3b6
AbhiGames10 <85124997+AbhiGames10@users.noreply.github.com>
2021-08-30 18:56:52 +0100

    Add files via upload

---
+++ Computer.js
@@ -0,0 +1,23 @@
+class Computer{
+    constructor(x,y,w,h){
+        var options={
+            isStatic:true
+        }
+
+        this.w = w;
+        this.h = h;
+
+        this.body = Bodies.rectangle(x,y,w,h,options);
+        this.image = loadImage("./assets/player.png");
+        World.add(world,this.body);
+    }
+
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.w,this.h);
+        pop();
+    }
+}
